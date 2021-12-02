5899de03c6d2c03090d3fc4a80c0a17cf256ec30
bill02928 <emile.tardif029@gmail.com>
2021-12-02 10:12:03 -0500

    final

--- lib/pk04_library/pk04_library.cpp
+++ lib/pk04_library/pk04_library.cpp
@@ -15,17 +15,15 @@ bool maxDistance;
 double maxSpeed;
 float pCorrection;
 int cycle, pulseCounterMaster, pulseCounterSlave, distanceMotor0, distanceMotor1, preError;
-char crecu;
+/*char crecu;
 //char i;
 char  incoming=0;
-char id_tag[20];
-
-
+char id_tag[11];*/
 int dernierCas[3] = {0}; 
 int nbCasUn = 0;
 
 //Implémentez vos fonction ici
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+//Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
 //Fonctions de mouvement pour le robot avec  PID
 void Avancer(float distance_voulue, float speed)
 {
@@ -64,7 +62,7 @@ void Avancer(float distance_voulue, float speed)
 
     vitesse_nouvelle = pid(ENCODER_Read(1), ENCODER_Read(0), vitesse);
     vitesse = vitesse_nouvelle;
-    delay(100); 
+    delay(50); 
   }
 
   MOTOR_SetSpeed(1, 0);
@@ -147,240 +145,7 @@ float pid(int nb_pulse_droite, int nb_pulse_gauche, float vitesse_droite)
   return vitesse_droite;
 }
 
-//Fonction pour actionner le servo
-//Le servo a deux positions possible, 0 ou 90 degrés.
-//le paramètre release définit l'état du servo.
-//Pour "catch" le servo va à un angle de 90 degrés.
-//Pour "release" (release = true) le servo va à un angle de 0 degrés.
-//Vous pouvez tweak l'angle en fonction de comment le servo est monté.
-void CatchNRelease(bool release){
-  int angle = 105;
-  if(release)
-  angle = 165;
 
-  SERVO_SetAngle(0,angle);
-  delay(300);
-
-}
-
-int Find()
-{ 
-    
-  float distance; 
-  distance= SONAR_GetRange(0); 
-Serial.print(distance);
-if  (distance > 40 || distance < 5 ) 
-{ 
-  return 0;
-}
-else{
-  return 1;
-}
-} 
-void frappe()
-{
-  
-digitalWrite(ledr, LOW);
-digitalWrite(ledb, LOW);
-digitalWrite(ledj, LOW);
-  digitalWrite(LedVerte, HIGH);
-  float distance = SONAR_GetRange(0);
-  Tourner(90, turn);
-  //delay(100);
-  SERVO_SetAngle(0,60); 
-  Avancer(distance-3, vroom + .2); 
-  yeet();
-  digitalWrite(LedVerte, LOW);
-
-  delay(50); 
-  
-  Avancer(distance-2, vroom ); 
-
-    Tourner(85, turn);
-
-}
-
-void DragNDropBall(int color)//rouge
-{  
-
-  if (color==1)
-  {
-    Tourner(2.5, turn);
-    Avancer(15, vroom - .15);
-    Avancer(50, vroom + .15);
-    Avancer(50, vroom + .15);
-    Avancer(50, vroom + .15);
-    Avancer(52, vroom + .15);
-    
-
-  }
-  if (color==2)//bleu
-{
-  Tourner(-90, turn);
-  delay(100);
-  Avancer(48, vroom -.15 );
-  Tourner(89, turn );
-  delay(100); 
-    Avancer(50, vroom + .1);
-    Avancer(50, vroom + .2);
-    Avancer(50, vroom + .2);
-    Avancer(50, vroom + .2);
-    Avancer(15, vroom + .1);
-   
-
-}
-  if (color==3)//jaune
-{
-  Tourner(90, turn );
-  delay(50); 
-  Avancer(45, vroom - .15 );
-  Tourner(-89.9, turn + .1); 
-  //delay(100);
-    Avancer(50, vroom + .2);
-    Avancer(50, vroom + .2);
-    Avancer(50, vroom + .2);
-    Avancer(50, vroom + .2);
-    Avancer(19, vroom );
- 
-}
-  delay(1500);
-  CatchNRelease(1); 
-}
-
-void RetourStart(int color)
-{
-     
-  if (color==1) //rouge
-  // retourne vers le circuit directement
-
-  {
-      Tourner(185, turn );
-
-  Avancer(50, vroom + .2);
-  Avancer(50, vroom + .2);
-  Avancer(50, vroom + .2);
-  Avancer(50, vroom + .2);
-  Avancer(50, vroom + .2);
-  Avancer(48, vroom + .1); // 48
-
-    Tourner(-90,turn + .15);
-
-  }
-  if (color==2) //bleu
-  // retourne vers le circuit directement(se dirige legerement vers le centre)
-
-{
-    Tourner(185, turn );
-
-  Avancer(50, vroom + .2);
-  Avancer(50, vroom + .2);
-  Avancer(50, vroom + .2);
-  Avancer(50, vroom + .1);
-
-
-  Tourner(-90, turn + .15);
-  
-  Avancer(20, vroom );
-
-  Tourner(95, turn + .15);
-    Avancer(50, vroom + .2);
-  Avancer(40, vroom + .2);
-  
-  Avancer(8, vroom );
-
-  Tourner(-90,turn );
-
-}
-  if (color ==3 )//jaune
-// retourne vers le circuit directement(se dirige legerement vers le centre)
-{
-    Tourner(184, turn );
-
-  Avancer(50, vroom + .1);
-  Avancer(50, vroom + .2);
-  Avancer(50, vroom + .2);
-  Avancer(50, vroom );
-
- 
-  Tourner(90, turn );
-   Avancer(40, vroom); //40
-
-  Tourner(-90, turn + .1);
-  Avancer(50, vroom + .2);
-  Avancer(40, vroom + .2);
-
-  Avancer(6, vroom + .1);
-
-  Tourner(-90,turn + .15);
-
-}
-}
-
-int couleur() {
-    uint16_t clear, red, green, blue;
-int color = 0;
-
-while (color == 0){
-  tcs.setInterrupt(false);      // turn on LED
-
-    delay(60);  // takes 50ms to read
-
-    tcs.getRawData(&red, &green, &blue, &clear);
-
-    tcs.setInterrupt(true);  // turn off LED
-   /* Serial.print(red);
-    Serial.print("\t");
-    Serial.print(green);
-    Serial.print("\t");
-    Serial.print(blue);
-    Serial.print("\n");*/
-
-if(600 <= red && red <= 750 && 500 <= green && green <= 649 && 600 <= blue && blue <= 750 )//rose
-{
-color = 1;
-}
-if(400 <= red && red <= 600 && 500 <= green && green <= 900 && 500 <= blue && blue <= 900 )//bleu
-{
-  color = 2;
-}
-if(600 <= red && red <= 750 && 650 <= green && green <= 850 && 500 <= blue && blue <= 750 )//jaune
-{
-  color = 3;
-}
-    delay(10);
-
-}
-if(color == 1)
-{
-
-  digitalWrite(ledr, HIGH);
-}
-if(color == 2)
-{
-  digitalWrite(ledb, HIGH);
-}if(color == 3)
-{
-  digitalWrite(ledj, HIGH);
-}
-return color;
-}
-
-int sifflet()
-{
-  
-if (analogRead(mic) > 575)
-{
-  
-  digitalWrite(ledr, HIGH);
-  digitalWrite(ledb, HIGH);
-  digitalWrite(ledj, HIGH);
-
-  return 1;
-}
-else{
-return 0;
-}
-}
 
 int capteurLigneCase(bool c, bool d, bool g){
   
@@ -445,120 +210,101 @@ void suiveurDeLigne(float *vitesseG, float *vitesseD){
 
     
     int z = capteurLigneCase((digitalRead(TEST2)), (digitalRead(TEST1)), (digitalRead(TEST3)));
+   
     reglerVitesse(z, vitesseG, vitesseD);
 
    MOTOR_SetSpeed(0, *vitesseG);
    MOTOR_SetSpeed(1, *vitesseD);
 
-    /*Serial.print("\n\n");
-    Serial.print("   cas: ");
-    Serial.print(z);
-    Serial.print("   VGauche:");
-    Serial.print(*vitesseG);
-    Serial.print("   VDroite:");
-    Serial.print(*vitesseD);*/
-
-};
-void yeet()
-{
-  
-  Tourner(90, .5);
-  Tourner(-270, .5);
-  //Tourner(90, .5);
-  SERVO_SetAngle(0, 165);
-  delay(50);
 
 
 }
-int scan()
-{
-int val = 0;
-int i;
-for ( i = 0 ; i < 100 ; i++)
-{
-val = analogRead(cote) + val;
-}
-val = val / i;
-val = val + 35 ;
-delay(500);
-return val;
-
-
-}
-
-void Avancerligne(float speed)
-{
-  float variationvit = 0.18;
-  float vitesse = speed - variationvit;
-  float vitesse_nouvelle = 0;
-  ENCODER_Reset(0);
-  ENCODER_Reset(1);
-
-  
-
-  while(digitalRead(TEST1) == 1 && digitalRead(TEST2) == 1)
-  {
-    
-  MOTOR_SetSpeed(0, .3);
-  MOTOR_SetSpeed(1, .3);
-
-  }
-
-  MOTOR_SetSpeed(1, 0);
-  MOTOR_SetSpeed(0, 0);
-  ENCODER_Reset(0);
-  ENCODER_Reset(1);
-
-}
-
-
-void rfid(){
-int i;
-int flag = 0;
-while(1){
-  
-    if( Serial2.available() )
-    {
-      crecu=Serial2.read();
-    
-        return;
-      }   // lit le ID-12
-      switch( crecu )
-      {
-        case 0x02:
-          // START OF TRANSMIT
-          AX_BuzzerON();
-          i=0;
-          incoming=1;
-          
-          break;
-        case 0x03:
-          // END OF TRANSMIT
-          AX_BuzzerOFF();
-          incoming=0;
-          
-          // Affiche le code recu sans valider le checksum
-          for( i=0 ; i<10 ; i++ )
-            Serial.print(id_tag[i]);
-            
-          Serial.println("");
-          
-          break;
-        default:
-          if( incoming )
-            id_tag[i++] = crecu;
-            
-          break;
-      }
-    }
-}
-
+ 
 
 void swince(){
   SERVO_Enable(0);
-  SERVO_SetAngle(0, 175);
+  SERVO_SetAngle(0, 75);
+  delay(50);
+  while(digitalRead(butred) == 0){
+  }
+  delay(1000);
+  SERVO_SetAngle(0, 178);
 delay(1000);
 SERVO_SetAngle(0, 75);
 delay(2000);
+  while(digitalRead(butred) == 0){
+  }
+  SERVO_SetAngle(0, 180);
+  delay(250);
 SERVO_Disable(0);
 }
+
+
+int button(){
+  while(1){
+    if(digitalRead(butvert) == HIGH){
+      return 1;
+    }
+        if(digitalRead(butred) == HIGH){
+      return 2;
+    }
+  }
+}
+
+int AlcooTest() {
+  //Vérifier que la pin est bien initialiser (A10)
+
+  //Pour éteindre le capteur quand pas utilisé
+  //digitalWrite(FonctionAlcool, HIGH);
+  Serial.print("Calibration de l'appareil.\n");
+  delay(1000);
+  Serial.print("Début du test.\n");
+  int ValeurAnalog[100]; //Tableau qui va permettre de trouver la valeur analog la plus haute
+  int MaxValeur = 0;
+  int i;
+  int cas; 
+
+  //Affiche "Traitement" à l'écran
+  //Vérifier que les librairies et l'écran sont bien installés/déclarés
+  screen(5);
+
+
+  //Boucle qui lit les valeurs durant un certain temps (à mesurer combien) et enregistre la valeur max
+  for(i = 0; i < 100; i++) {
+    ValeurAnalog[i] = analogRead(PinAlcool);
+    Serial.print(ValeurAnalog[i]);
+    Serial.print("\n");
+
+    if (ValeurAnalog[i] > MaxValeur) {
+      MaxValeur = ValeurAnalog[i];
+    }
+    delay(20);
+  }
+
+  Serial.print("\n\n\n\n");
+  Serial.print(MaxValeur);
+  Serial.print("\n\n\n\n");
+
+  
+
+  if (MaxValeur <= 200) {
+    cas = 1;
+  }
+  else if (MaxValeur > 200 && MaxValeur <= 300) {
+    cas = 2;
+  }
+  else if (MaxValeur > 300 && MaxValeur <= 400) {
+    cas = 3;
+  }
+  else if (MaxValeur > 400 && MaxValeur <= 1024) {
+    cas = 4;
+  }
+
+  Serial.print("\n\n\n\n");
+  Serial.print(cas);
+  Serial.print("\n\n\n\n");
+
+  Serial.print("Fin du test.\n");
+  return cas;
+}
+
--- src/main.ino
+++ src/main.ino
@@ -19,7 +19,7 @@ Variables globales et defines
 // -> defines...
 // L'ensemble des fonctions y ont acces
 
-
+Adafruit_SH1106 oled(-1);
 
 /* ****************************************************************************
 Vos propres fonctions sont creees ici
@@ -36,24 +36,97 @@ Fonctions d'initialisation (setup)
 
 void setup(){
   BoardInit();
-
+  Serial.begin(9600);
+    oled.begin(SH1106_SWITCHCAPVCC, 0x3C);
+    oled.clearDisplay();
+    oled.display();
     pinMode(redpin, OUTPUT);
     pinMode(greenpin, OUTPUT);
     pinMode(bluepin, OUTPUT);
-    pinMode(ledr, OUTPUT);
-    pinMode(ledb, OUTPUT);
-    pinMode(ledj, OUTPUT);
-    pinMode(LedVerte, OUTPUT);
-    pinMode(mic, INPUT);
+    pinMode(pos1, INPUT);
+    pinMode(pos2, INPUT);
+    pinMode(pos3, INPUT);
     pinMode(TEST1, INPUT);//Droite
     pinMode(TEST2, INPUT);//Milieu
     pinMode(TEST3, INPUT);//Gauche
     pinMode(cote, INPUT);//cote
+    pinMode(butvert, INPUT);
+    pinMode(poids, INPUT);
+    pinMode(PinAlcool, INPUT);
+    pinMode(FonctionAlcool, OUTPUT);
+    digitalWrite(FonctionAlcool, LOW);
     SERVO_Enable(0);
-    SERVO_SetAngle(0,75);
-  Serial2.begin(9600);
+    SERVO_SetAngle(0,180);
+    delay(200);
+//  Serial2.begin(9600);
   Serial.println("Test du ID-12 sur UART2 (RX2/Digital 17)");
 SERVO_Disable(0);
+  oled.begin(SH1106_SWITCHCAPVCC, 0x3C);
+
+  oled.clearDisplay();
+}
+void screen(int texte) {
+ 
+ switch(texte) {
+    case 1:
+      oled.clearDisplay();
+      oled.setTextSize(2);
+      oled.setTextColor(WHITE);
+      oled.setCursor(0,29);
+      oled.print("Sobre");
+      oled.display();
+      delay(1000);
+      //oled.clearDisplay();
+      //oled.display();
+      break;
+    case 2:
+      oled.clearDisplay();
+      oled.setTextSize(2);
+      oled.setTextColor(WHITE);
+      oled.setCursor(0,29);
+      oled.print("Bu");
+      oled.display();
+      delay(1000);
+      //oled.clearDisplay();
+      //oled.display();
+      break;
+    case 3:
+      oled.clearDisplay();
+      oled.setTextSize(2);
+      oled.setTextColor(WHITE);
+      oled.setCursor(0,29);
+      oled.print("Chaudasse");
+      oled.display();
+      delay(1000);
+      //oled.clearDisplay();
+      //oled.display();
+      break;
+    case 4:
+      oled.clearDisplay();
+      oled.setTextSize(2);
+      oled.setTextColor(WHITE);
+      oled.setCursor(0,29);
+      oled.print("Bateau");
+      oled.display();
+      delay(1000);
+      //oled.clearDisplay();
+      //oled.display();
+      break;
+    case 5:
+      oled.clearDisplay();
+      oled.setTextSize(2);
+      oled.setTextColor(WHITE);
+      oled.setCursor(0,29);
+      oled.print("Traitement");
+      oled.display();
+      delay(1000);
+      //oled.clearDisplay();
+      //oled.display();
+      break;
+  }
+  delay(5000);
+     oled.clearDisplay();
+    oled.display();
 }
 
 /* ****************************************************************************
@@ -62,11 +135,33 @@ Fonctions de boucle infini (loop())
 // -> Se fait appeler perpetuellement suite au "setup"
 
 void loop() {
+
+float vitesseG = VITESSE;
+  float vitesseD = VITESSE;
+ int flag = 0; 
+ int action = 0;
+oled.drawBitmap(0,0, bitmap_mcdo , 128 , 64 , WHITE);
+oled.display();
  
- // rfid();
-  delay(15000);
+while(flag == 0){
+  suiveurDeLigne(&vitesseG, &vitesseD);
+  flag = digitalRead(pos1);
+  Serial.print(flag);
+}
+MOTOR_SetSpeed(0,0);
+MOTOR_SetSpeed(1,0);
+delay(100);
+
+action = button();
+delay(100);
+if(action == 1){
   swince();
-  delay(2000);
-  SERVO_Disable(0);
-  exit(0);
+  delay(1000);
+  }
+if(action == 2){
+  oled.clearDisplay();
+  screen(AlcooTest());
+  delay(4000);
+}
+delay(100);
 }
